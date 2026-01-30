3d25980df097c4cb6593cd61cb63786d2221ed46
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2026-01-30 13:45:10 -0500

    Added input package and basic script
    -Super simple input setup that allows for basic arrow movement

---
+++ PawnPuzzle/Assets/Scripts/PlayerScripts/InputDetector.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputDetector : MonoBehaviour
+{
+    private Vector2 movementInput;
+    public Vector2 MovmentInput
+    { get { return movementInput; } }
+
+    private PlayerInputs playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = new PlayerInputs();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput = playerInput.BasicMovement.WASD.ReadValue<Vector2>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+}
